70afaca0754fa1866200cb10815bdd0a514c3b2a
michele <miccol@kth.se>
2017-06-08 11:02:12 +0200

    figured out how to make dynamic widget

--- bt/NewDraw.py
+++ bt/NewDraw.py
@@ -1,15 +1,16 @@
 import gi
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GdkPixbuf, Gdk, GLib
 
 import xdot
 from NodeStatus import *
 from threading import RLock
 from BehaviorTree import *
+from gi.repository import GObject
 
-
-
+import random
+from time import sleep
 
 
 class MyDotWindow(xdot.DotWindow):
@@ -27,66 +28,54 @@ digraph G {
 }
 """
 
-# std::string DotBt::defineNodeDot(TreeNode* node, const std::string& alias)
-# {
-#   std::string output = alias + " ";
-#
-#   // Find the type of the node and its shape and symbol (label).
-#   switch (node->DrawType())
-#   {
-#     case SELECTORSTAR:
-#       output += "[label=\"*\n?\" penwidth=\"2\"  shape=\"box\"";
-#       break;
-#     case BT::SEQUENCESTAR:
-#       output += "[label=\"*\n-->\" penwidth=\"2\"  shape=\"box\"";
-#       break;
-#     case BT::SELECTOR:
-#       output += "[label=\"?\" penwidth=\"2\"  shape=\"box\"";
-#       break;
-#     case BT::SEQUENCE:
-#       output += "[label=\"-->\" penwidth=\"2\"  shape=\"box\"";
-#       break;
-#     case BT::PARALLEL:
-#       output += "[label=\"-->\n-->\" penwidth=\"2\"  shape=\"box\"";
-#       break;
-#     case BT::DECORATOR:
-#       output += "[label=\"D\" penwidth=\"2\" shape=\"diamond\"";
-#       break;
-#     case BT::ACTION:
-#       output += "[label=\"" + node->get_name() + "\" penwidth=\"2\" shape=\"box\" fillcolor=\"palegreen\" style=\"filled\"";
-#       break;
-#     case BT::CONDITION:
-#       output += "[label=\"" + node->get_name() + "\" penwidth=\"2\" shape=\"ellipse\" fillcolor=\"khaki1\" style=\"filled\"";
-#       break;
-#     default:
-#       break;
-#   }
-#
-#   // Get the current status of the node for the coloring.
-#   switch (node->get_color_status())
-#   {
-#     case BT::RUNNING:
-#       output += " color=\"black\" ];";
-#       break;
-#     case BT::SUCCESS:
-#       output += " color=\"green\" ];";
-#       break;
-#     case BT::FAILURE:
-#       output += " color=\"red\" ];";
-#       break;
-#     case BT::IDLE:
-#       output += " color=\"gray88\" ];";
-#       break;
-#     case BT::HALTED:
-#       output += " color=\"orange\" ];";
-#       break;
-#     default:
-#       output += " color=\"gray88\" ];";
-#       break;
-#   }
-#
-#   return output;
-# }
+
+class BTWindow(xdot.DotWindow):
+    def __init__(self):
+        xdot.DotWindow.__init__(self)
+        self.dotcode = None
+
+
+    def get_dot_code(self):
+
+        return self.dotcode
+
+    def update_widget(self):
+        print('update')
+
+        print('getting code')
+        symbols = ['black', 'red', 'yellow']
+        random.seed
+        symbol = random.choice(symbols)
+        self.dotcode = """
+        digraph G {
+            World[label=
+            """ + symbol + """
+            penwidth=\"2\"  shape=\"box\" color=\"black\" ]
+            Hello -> World
+            Ciao -> World
+            Ciao2 -> World
+            Ciao3 -> World
+        }
+        """
+
+
+        GLib.timeout_add_seconds(1, self.update_widget)
+        self.set_dotcode(self.dotcode)
+        pass
+
+
+
+def draw():
+    window = BTWindow()
+    window.update_widget()
+    # dcode = window.get_dot_code()
+    # window.set_dotcode(dcode)
+    window.connect('delete-event', Gtk.main_quit)
+
+    #Gtk.timeout_add(250, update_widget)  # call update_widget every 250 ms
+    #Gtk.main_iteration_do(True)
+
+    Gtk.main()
 
 
 
@@ -98,89 +87,64 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    draw()
 
 
-# def updateDraw(canvas,tree,xPos, yPos,xPosParent,yPosParent,depth):
-#     #print 'class '+ tree.nodeClass
-#     #print 'type '+ tree.nodeType
+
+
+# from gi.repository import Gtk, GdkPixbuf, Gdk, GLib
+# import os, sys
+# import time
 #
+# class GUI:
+#     def __init__(self):
+#         self.window=Gtk.Window()
+#         self.window.set_title("Countdown")
+#         self.window.set_border_width(10)
+#         self.window.connect_after('destroy', self.destroy)
 #
-#     if tree.nodeClass == 'Leaf':
-#         drawNode(canvas,tree,xPos,yPos,xPosParent,yPosParent)
-#     else:
-#         drawNode(canvas,tree,xPos,yPos,xPosParent,yPosParent)
-#         yPos_ = yPos + 50
-#         xPos_ = xPos
-#         childrenWidth = getDistance()*depth
-#         distance = 30
-#         nOfChild = len(tree.Children)
-#         for c in tree.Children:
-#             childrenWidth = childrenWidth + max(8*len(c.name)+4,50) + distance
-#             offset = 0
-#             if len(tree.Children)%2 == 0:
-#                 offset = 4*len(tree.Children) + 4
-#             else:
-#                 offset = 4*(len(tree.Children)-1) + 4
-#             xPos_ = xPos - childrenWidth/2 -distance - nOfChild*offset - getDistance()*depth
-#         for c in tree.Children:
-#             xPos_ = xPos_ + max(8*len(c.name)+4,50) + distance + getDistance()*depth + offset
-#             updateDraw(canvas,c,xPos_,yPos_,xPos,yPos,depth/2)
+#         # main container of projct
+#         self.main_box=Gtk.VBox()
+#         self.main_box.set_spacing(5)
 #
-# def init(canvas,tree,root):
-#     redrawAll(canvas,tree,root)
-#     #updateDraw(canvas,tree,400,10, -1,-1)#400 is in the middle of the canvas (800x600)
+#         # countdown label
+#         self.countdown_label = Gtk.Label()
 #
-# def drawNode(canvas, tree,xPos,yPos,xPosParent,yPosParent):
+#         # start button
+#         self.start_button=Gtk.Button("Start!")
 #
-#     if tree.GetColor() == NodeColor.Gray:
-#         nodeColor = 'gray'
-#     elif tree.GetColor() == NodeColor.Green:
-#         nodeColor = 'green'
-#     elif tree.GetColor() == NodeColor.Red:
-#         nodeColor = 'red'
-#     else:
-#         nodeColor = 'black'
+#         # add the elements to the window
+#         self.window.add(self.main_box)
+#         self.main_box.pack_start(self.countdown_label, False, False, 0)
+#         self.main_box.pack_start(self.start_button, False, False, 0)
 #
+#         # connect buttons
+#         self.start_button.connect_after('clicked', self.start_clicked)
 #
-#     if tree.nodeType == 'Sequence':
-#         #print 'Drawing a sequence'
-#         xSize = 15
-#         ySize = 15
-#         canvas.create_rectangle(0+xPos-xSize/2,  0+yPos-ySize/2, 15+xPos-xSize/2, 15+yPos-ySize/2, outline=nodeColor, stipple="")
-#         canvas.create_text(xPos,yPos,fill="black",font="Times 15 bold",
-#                         text=u'\u2192')
-#         if xPosParent > 0:#avoid drawing the first line
-#             canvas.create_line(xPosParent,yPosParent+7.5, xPos, yPos -ySize/2)
+#         self.window.show_all()
 #
-#     if tree.nodeType == 'Selector':
-#         #print 'Drawing a selector'
-#         xSize = 15
-#         ySize = 15
-#         canvas.create_rectangle(0+xPos-xSize/2,  0+yPos-ySize/2, 15+xPos-xSize/2, 15+yPos-ySize/2, outline=nodeColor, stipple="")
-#         canvas.create_text(xPos,yPos,fill="black",font="Times 15 bold",
-#                         text='?')
-#         if xPosParent > 0:#avoid drawing the first line
-#             canvas.create_line(xPosParent,yPosParent+7.5, xPos, yPos -ySize/2)
+#     def start_clicked(self, button):
+#         self.countdown(3)
 #
+#     def countdown(self, count):
+#         if count > 0:
+#             self.countdown_label.set_text(str(count))
+#             # Manually re-add this callback with a decremented count.
+#             GLib.timeout_add_seconds(2, self.countdown, count - 1)
+#         else:
+#             self.countdown_label.set_text("Go!")
 #
-#     if tree.nodeType == 'Action':
-#         #print 'Drawing an action'
-#         xSize = max(8*len(tree.name)+4,50)
-#         ySize = 20
-#         canvas.create_rectangle(xPos-xSize/2,  yPos-ySize/2, xSize+xPos-xSize/2, ySize+yPos - ySize/2, outline=nodeColor,fill='green', stipple="")
-#         canvas.create_text(xPos,yPos,fill="black",font="Times 15 bold",
-#                         text=tree.name )
-#         canvas.create_line(xPosParent,yPosParent + 7.5, xPos, yPos - ySize/2)
+#         # Return False so the callback is not called repeatedly
+#         # as we manually re-added the callback ourselves above.
+#         return False
 #
-#     if tree.nodeType == 'Condition':
-#         #print 'Drawing a Condition'
-#         xSize = max(8*len(tree.name)+4,50)
-#         ySize = 20
-#         #canvas.create_rectangle(xPos,  yPos, xSize+xPos, ySize+yPos, outline="black",fill='green', stipple="")
+#     def destroy(window, self):
+#         Gtk.main_quit()
 #
-#         #canvas.create_oval(xPos, yPos, xSize+xPos, ySize+yPos, fill="yellow", stipple="")
-#         canvas.create_oval(xPos-xSize/2,  yPos-ySize/2, xSize+xPos-xSize/2, ySize+yPos - ySize/2, outline=nodeColor,fill='yellow', stipple="")
-#         canvas.create_text(xPos,yPos,fill="black",font="Times 15 bold",
-#                         text=tree.name)
-#         canvas.create_line(xPosParent,yPosParent + 7.5, xPos, yPos - ySize/2)
+# def main():
+#     app=GUI()
+#     Gtk.main()
+#
+# if __name__ == "__main__":
+#     sys.exit(main())
